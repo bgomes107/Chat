882a42249a2299fabe13d3c73dc6029e8792ebc8
Bruno Gomes de Oliveira <111610862+bgomes107@users.noreply.github.com>
2022-08-25 14:23:22 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,19 @@
-// import {initializeApp, getApp} from 'firebase/app';
-// import {initializeFirestore} from 'firebase/firestore';
-// import {getAuth} from 'firebase/auth';
+import {initializeApp, getApp} from 'firebase/app';
+import {initializeFirestore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCKc3tsRbf5Rq6DUtgbQJ8T56KtW59YxR4",
-//     authDomain: "messenger-ee60d.firebaseapp.com",
-//     projectId: "messenger-ee60d",
-//     storageBucket: "messenger-ee60d.appspot.com",
-//     messagingSenderId: "569356861829",
-//     appId: "1:569356861829:web:9eb15e3e493964f9a404ec"
-//   };
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
 
-//   const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
 
-//   const auth = getAuth(app);
-//   const db = initializeFirestore(app, {experimentalForceLongPolling: true});
+  const auth = getAuth(app);
+  const db = initializeFirestore(app, {experimentalForceLongPolling: true});
 
-//   export {db, auth};
+  export {db, auth};
